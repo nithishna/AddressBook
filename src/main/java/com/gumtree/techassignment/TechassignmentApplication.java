e9e30052c352b70e876721308b1d489fda4e92e3
Sireesha padi <padi@Sireeshas-MacBook-Air.local>
2022-12-01 13:41:51 +0000

    Initialized the spring boot project

---
+++ src/main/java/com/gumtree/techassignment/TechassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.gumtree.techassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechassignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gumtree/techassignment/TechassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumtree.techassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
