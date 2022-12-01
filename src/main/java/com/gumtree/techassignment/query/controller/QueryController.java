419c1f0ee0e44cf98fc1547421529acbaa188628
nithishna <nithishna@gmail.com>
2022-12-01 23:39:25 +0000

    Added logger to the controller and service layer

--- src/main/java/com/gumtree/techassignment/query/controller/QueryController.java
+++ src/main/java/com/gumtree/techassignment/query/controller/QueryController.java
@@ -5,6 +5,8 @@ import com.gumtree.techassignment.query.dto.Person;
 import com.gumtree.techassignment.query.service.QueryService;
 import com.gumtree.techassignment.query.validator.AgeComparisonInputValidator;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -14,22 +16,28 @@ import java.text.ParseException;
 @RequestMapping("/query")
 @RequiredArgsConstructor
 public class QueryController {
+    private static final Logger logger = LoggerFactory.getLogger(QueryController.class);
+    
     private final AgeComparisonInputValidator validator;
     private final QueryService service;
 
     @PostMapping("/age-comparison")
     public long ageComparison(@RequestBody AgeComparisonRequest inputRequest) throws IOException, ParseException {
+        logger.info("ageComparison - start");
         validator.validateInput(inputRequest);
+        logger.debug("ageComparison - validation success");
         return service.ageComparison(inputRequest);
     }
 
     @GetMapping("/gender/{value}/count")
     public long numberOfPersonByGender(@PathVariable("value") String value) {
+        logger.info("numberOfPersonByGender - start");
         return service.getNumberOfPersonByGender(value);
     }
 
     @GetMapping("/oldest-person")
     public Person getOldestPerson() {
+        logger.info("getOldestPerson - start");
         return service.getOldestPerson();
     }
 }
--- src/main/java/com/gumtree/techassignment/query/service/QueryService.java
+++ src/main/java/com/gumtree/techassignment/query/service/QueryService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import com.gumtree.techassignment.query.dto.AgeComparisonRequest;
 import com.gumtree.techassignment.query.dto.Person;
 import com.gumtree.techassignment.query.filehandling.PersonTextFileLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -16,20 +18,26 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class QueryService {
+    private static final Logger logger = LoggerFactory.getLogger(QueryService.class);
+
     private final PersonTextFileLoader personTextFileLoader;
     private final Map<String, List<Integer>> firstNameIndex;
 
     public long ageComparison(AgeComparisonRequest inputRequest) throws IOException, ParseException {
+        logger.debug("ageComparison service - start");
         Person firstPerson = retrieveFromAddressBook(inputRequest.getSource());
         Person secondPerson = retrieveFromAddressBook(inputRequest.getTarget());
+        logger.debug("Retrieved the person details from addressBook");
         return ChronoUnit.DAYS.between(firstPerson.getDateOfBirth().toInstant(), secondPerson.getDateOfBirth().toInstant());
     }
 
     public long getNumberOfPersonByGender(String value) {
+        logger.debug("getNumberOfPersonByGender service - start");
         return personTextFileLoader.getCountByGender(value);
     }
 
     public Person getOldestPerson() {
+        logger.debug("getOldestPerson service - start");
         return personTextFileLoader.getOldestPerson();
     }
 
@@ -37,22 +45,27 @@ public class QueryService {
     * we don't have to deal with the edge cases here as the AgeComparisonInput Validator has taken care of those
     * */
     private Person retrieveFromAddressBook(Person input) throws IOException, ParseException {
+        logger.debug("retrieveFromAddressBook - start");
         List<Integer> indices = firstNameIndex.get(input.getFirstName());
         Person result = null;
         if(indices.size() == 1) {
+            logger.debug("retrieveFromAddressBook - one index");
             result = personTextFileLoader.findPersonByLineNumber(indices.get(0));
         }
         else {
+            logger.debug("retrieveFromAddressBook - multiple indices");
             //if there is ambiguity based on first name use the optional parameter to land a unique person.
             //There is no need to check whether optional parameter is present - it is taken care in the validator itself. If the control flow reaches here then it means the optional parameter do exist
             for (int index : indices) {
                 Person retrievedPerson = personTextFileLoader.findPersonByLineNumber(index);
                 if(StringUtils.hasText(input.getLastName()) && input.getLastName().equalsIgnoreCase(retrievedPerson.getLastName())) {
                     result = retrievedPerson;
+                    logger.debug("retrieveFromAddressBook - multiple indices | exact match found by lastName");
                     break;
                 }
                 if(input.getDateOfBirth() != null && input.getDateOfBirth().equals(retrievedPerson.getDateOfBirth())) {
                     result = retrievedPerson;
+                    logger.debug("retrieveFromAddressBook - multiple indices | exact match found by dob");
                     break;
                 }
             }
